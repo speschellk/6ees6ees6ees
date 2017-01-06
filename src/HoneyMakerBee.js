fd1e814e22e6ad5afe78c6cb0d36f3da544df157
Kay Christensen <kaychristensen@gmail.com>
2017-01-06 10:50:14 -0800

    Complete HoneyMakerBee subclass

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,10 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, color, food, eat) {
+    super(color, food, eat);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    this.makeHoney = () => { this.honeyPot++; }
+    this.giveHoney = () => { this.honeyPot--; }
+  }
 };
