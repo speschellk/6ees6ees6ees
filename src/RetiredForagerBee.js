d05e4d4d55fae007c6027f41ab74e60cc3586ac5
Kay Christensen <kaychristensen@gmail.com>
2017-01-06 11:01:34 -0800

    Complete RetiredForagerBee subclass

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,11 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, job, color, canFly, food, eat, treasureChest) {
+    super(food, eat, treasureChest);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    this.forage = () => { return 'I am too old, let me play cards instead' };
+    this.gamble = () => { this.treasureChest.push('treasure'); }
+  }
 };
