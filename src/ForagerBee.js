e04267cbab0b79b067a4c9778519bfed6f6caa49
Kay Christensen <kaychristensen@gmail.com>
2017-01-06 10:54:05 -0800

    Complete ForagerBee subclass

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(age, color, food, eat) {
+    super(color, food, eat);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+    this.forage = () => { this.treasureChest.push('treasure'); }
+  }
 };
